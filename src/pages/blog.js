51ca80706d8826ee22ce927ff8524b470a86faf9
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-01-28 12:38:06 +0800

    using css module, changing file name and called by reference, not required extra dependencies

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import styles from '../components/blog.module.css';
 
 class Blog extends Component {
   render() {
     return (
       <Layout>
-        <h1>This is blog page</h1>
+        <h1 className={styles.title}>This is blog page</h1>
         <div>
           {/*<a href="/">Home Page</a>*/}
           <Link to="/">home page</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,11 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <h1>
+    <h1 className="title">
       Hello, This is our home page
+    </h1>
+    <h1 className="title">
+      Another heading
     </h1>
     <div>
       {/*<a href="/blog/">blog page</a>*/}
