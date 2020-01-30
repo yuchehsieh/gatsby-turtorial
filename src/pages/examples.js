e8fd95efd931629b9c37ff71eb3f0763ca9956a7
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-01-30 12:32:44 +0800

    using PageQuery to get data, only page-component can use PageQuerygit add .

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,15 +1,34 @@
 import React from "react";
+import {graphql} from 'gatsby';
 import HeaderHooks from '../examples/header-hooks';
 import HeaderStatic from "../examples/header-static"
 
-const Examples = () => {
+const Examples = (props) => {
+  console.log(props);
   return (
     <div>
       examples page
       <HeaderHooks/>
       <HeaderStatic />
+      <hr />
+      <h4>Page Query Result: </h4>
+      <p>{props.data.site.siteMetadata.description}</p>
+
     </div>
   )
 }
+
+export const getData = graphql
+  `
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+ `
 
 export default Examples;
