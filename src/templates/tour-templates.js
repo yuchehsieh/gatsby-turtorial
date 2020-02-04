b1b1be0178bea4749980f5996059d1afb64da7fb
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-02-04 20:49:32 +0800

    querying the tour by slug through pageContext(automatically passed as query parameter)

--- src/templates/tour-templates.js
+++ src/templates/tour-templates.js
@@ -1,14 +1,51 @@
-import React from "react"
+import React from "react";
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import Image from 'gatsby-image';
+import { FaMoneyBillWave, FaMap }  from 'react-icons/fa';
+import styles from '../css/template.module.css';
+import Layout from "../components/layout"
 
 const TourTemplates = (props) => {
 
   console.log(props);
 
   return (
-    <div>
+    <Layout>
       hello from tour template!
-    </div>
+      <Image fluid={props.data.tour.images[0].fluid}/>
+    </Layout>
   )
 }
+
+/**
+ *
+ * the $slug is automatically passed through pageContext!
+ *
+ */
+
+export const query = graphql`
+  query($slug: String!) {
+    tour: contentfulTour(slug: {eq: $slug}) {
+      name
+      journey {
+        day
+        info
+      }
+      price
+      description {
+        description
+      }
+      country
+      slug
+      days
+      images {
+        fluid {
+          ...GatsbyContentfulFluid_withWebp
+        }
+      }
+      start(formatString: "dddd MMMMM Do, YYYY")
+    }
+  }
+`
 
 export default TourTemplates
