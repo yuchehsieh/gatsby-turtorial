e6391f134a7eed1be13aba8304c6f30469e761a9
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-02-04 21:10:14 +0800

    creating layout of each tour-template-page

--- src/templates/tour-templates.js
+++ src/templates/tour-templates.js
@@ -1,18 +1,46 @@
-import React from "react";
-import { useStaticQuery, graphql, Link } from 'gatsby';
-import Image from 'gatsby-image';
-import { FaMoneyBillWave, FaMap }  from 'react-icons/fa';
-import styles from '../css/template.module.css';
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Image from "gatsby-image"
+import { FaMap, FaMoneyBillWave } from "react-icons/fa"
+import styles from "../css/template.module.css"
 import Layout from "../components/layout"
 
-const TourTemplates = (props) => {
+const TourTemplates = ({ data }) => {
 
-  console.log(props);
+  const { name, price, images, description: { description }, days, start, country, journey } = data.tour
 
   return (
     <Layout>
-      hello from tour template!
-      <Image fluid={props.data.tour.images[0].fluid}/>
+      <section className={styles.template}>
+        <div className={styles.center}>
+          <div className={styles.images}>
+            {images.map((item, index) => (
+              <Image fluid={item.fluid} key={index} alt={name} className={styles.image}/>
+            ))}
+          </div>
+          <h2>{name}</h2>
+          <div className={styles.info}>
+            <p>
+              <FaMoneyBillWave className={styles.icon}/>
+              starting from ${price}
+            </p>
+            <p>
+              <FaMap className={styles.icon}/>
+              {country}
+            </p>
+          </div>
+          <h4>starts on : {start}</h4>
+          <h4>duration : {days} days</h4>
+          <p className={styles.desc}>{description}</p>
+          <h2>daily schedule</h2>
+          <ul className={styles.journey}>
+            {journey.map((item, index) => (
+              <li key={index}>{item.day}</li>
+            ))}
+          </ul>
+          <Link to="/tours">back to tours</Link>
+        </div>
+      </section>
     </Layout>
   )
 }
@@ -20,6 +48,9 @@ const TourTemplates = (props) => {
 /**
  *
  * the $slug is automatically passed through pageContext!
+ *
+ * query parameter using $
+ * mark ! as @required
  *
  */
 
